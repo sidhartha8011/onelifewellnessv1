f53e4e0cb8d6a0d15e06b35e7731ceb2e572ff9a
Accenous-Integrals <accenousdev@gmail.com>
2024-12-20 00:32:16 +0530

    first deployment pust

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     outDir: 'dist', // Change the output folder from 'dist' to 'build'
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`, // Automatically inject React for JSX
   },
 }); 
